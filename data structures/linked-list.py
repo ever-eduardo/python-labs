9ea78961afe972de17c421cccdf5a47a5e193122
everness <106024807+everness-dev@users.noreply.github.com>
2022-06-22 20:49:16 -0500

    Update linked-list.py

--- data structures/linked-list.py
+++ data structures/linked-list.py
@@ -10,7 +10,7 @@ class Node:
     """
     __slots__ = 'data', 'next'
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Initializes the Node first state.
         :param data: Any
@@ -24,11 +24,48 @@ class LinkedList:
     """
     Class modeling a singly linked list.
     """
-    __slots__ = 'head', 'size'
+    __slots__ = 'head', 'tail', 'size'
 
     def __init__(self):
         """
-        Initializes a list to its initial state with zero nodes and size of zero.
+        Initializes a list to its initial state with head and tail to none and size to zero.
         """
-        self.head = Node
+        self.head = None
+        self.tail = None
         self.size = 0
+
+    def __len__(self):
+        """
+        :return: The number of elements in the list.
+        """
+        return self.size
+
+    def is_empty(self):
+        """
+        :return: True when the list has no nodes.
+        """
+        return self.size == 0
+
+    def push_front(self, data):
+        """
+        Pushes a node at the front-end of the list.
+        :param data: Any
+        :return: None
+        """
+        if self.tail is None and self.head is None:
+            node = Node(data)
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            node = Node(data)
+            node.next = self.head
+            self.head = node
+            self.size += 1
+
+    def push_back(self, data):
+        """
+        Pushes a node at the back-end of the list.
+        :param data: Any
+        :return: None
+        """
