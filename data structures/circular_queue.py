815fbc9ed70ab7a474d827339baee9149b67230c
everness <106024807+everness-dev@users.noreply.github.com>
2022-06-23 12:15:18 -0500

    Update circular_queue.py

--- data structures/circular_queue.py
+++ data structures/circular_queue.py
@@ -26,5 +26,63 @@ class CircularQueue:
     __slots__ = 'tail', 'size'
 
     def __init__(self):
+        """
+        Initializas an empty Queue.
+        """
         self.tail = None
         self.size = 0
+
+    def __len__(self):
+        """
+        :return: The number of elements in the queue.
+        """
+        return self.size
+
+    def is_empty(self):
+        """
+        :return: True when the queue has no nodes.
+        """
+        return self.size == 0
+
+    def first(self):
+        """
+        Allows to peek the data of the first element of the Queue.
+        Raises an exception when the Queue is empty.
+        :return: The data of the first element of the Queue.
+        """
+        if self.is_empty():
+            raise Exception("Attempting to peek an element from an empty queue")
+        return self.tail.next.data
+
+    def enqueue(self, data):
+        """
+        Adds an element to the tail of the Queue.
+        :param data: Any
+        :return: None
+        """
+        node = Node(data)
+        if self.is_empty():
+            node.next = node
+        else:
+            node.next = self.tail.next
+            self.tail.next = node
+        self.tail = node
+        self.size += 1
+
+    def dequeue(self):
+        """
+        Removes the first element of the Queue.
+        Raises an exception when the queue is empty.
+        :return: The data of the first element of the Queue.
+        """
+        if self.is_empty():
+            raise Exception("Attempting to dequeue an element from an empty Queue")
+        current_head = self.tail.next
+        data = current_head.data
+        if self.size == 1:
+            self.tail = None
+        else:
+            self.tail.next = current_head.next
+        current_head.next = None
+        self.size -= 1
+        return data
