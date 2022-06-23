aae050e569b25d713a9ed5ba538af74a01b1bed7
everness <106024807+everness-dev@users.noreply.github.com>
2022-06-23 11:54:43 -0500

    Update circular_queue.py

--- data structures/circular_queue.py
+++ data structures/circular_queue.py
@@ -1,1 +1,30 @@
+#
+# Circular Queue FIFO ADT
+# Circularly Linked List Implementation.
+#
 
+class Node:
+    """
+    Class modeling a Node for a linked style ADT.
+    """
+    __slots__ = 'data', 'next'
+
+    def __init__(self, data, next_node=None):
+        """
+        Initializes the Node.
+        :param data: Any
+        :param next_node: Any
+        """
+        self.data = data
+        self.next = next_node
+
+
+class CircularQueue:
+    """
+    Queue implementation using a circular singly linked list.
+    """
+    __slots__ = 'tail', 'size'
+
+    def __init__(self):
+        self.tail = None
+        self.size = 0
