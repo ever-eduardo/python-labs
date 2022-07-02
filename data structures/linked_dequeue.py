ca796186c9f3b5f297aaa8a91b541de2aceec29c
everness <106024807+everness-dev@users.noreply.github.com>
2022-07-01 21:56:23 -0500

    Created the class LinkedDequeue with basic methods

--- data structures/linked_dequeue.py
+++ data structures/linked_dequeue.py
@@ -20,3 +20,26 @@ class Node:
         self.data = data
         self.prev = prev
         self.next = next_node
+
+
+class LinkedDequeue:
+    """
+    Double ended queue ADT.
+    A Doubly Linked List Implementation.
+    """
+
+    def __init__(self):
+        """
+        Creates an empty dequeue.
+        """
+        self.header = Node(None, None, None)
+        self.trailer = Node(None, None, None)
+        self.header.next = self.trailer
+        self.trailer.prev = self.header
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
