0bf39bea1fa01ab60567e2207c6b072a7a31f763
everness <106024807+everness-dev@users.noreply.github.com>
2022-07-30 18:47:56 -0500

    Deleted quotes

--- web development/django/quotes/quotes/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Quote
-
-
-# Create your views here.
-def show_quotes(request):
-    quotes = Quote.objects.all()
-    return render(request, 'quotes/index.html', {'quotes': quotes})
-
