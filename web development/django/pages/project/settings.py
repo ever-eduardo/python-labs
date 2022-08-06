32ee8a125ee4cb012a31acd8a7367948c02174e0
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-06 15:17:08 -0500

    New updates for Heroku deployment

--- web development/django/pages/project/settings.py
+++ web development/django/pages/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&es9teg5ldqt2v@*f55b)&=co-!!qlk9d(@aul34#^uy_li=)o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
