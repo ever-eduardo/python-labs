8cec5ff0eecc6266f644adf06339565489be3715
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-06 11:50:58 -0500

    Added a new about page with view, url and template

--- web development/django/pages/pages/views.py
+++ web development/django/pages/pages/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name: str = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name: str = "about.html"
