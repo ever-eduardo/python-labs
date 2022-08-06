f5ae923258beb39fb319f7f2585a5da70b438d59
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-06 14:57:08 -0500

    Added a url name test

--- web development/django/pages/pages/tests.py
+++ web development/django/pages/pages/tests.py
@@ -1,13 +1,22 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
 class HomePageTest(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        respose = self.client.get(reverse("home"))
+        self.assertEqual(respose.status_code, 200)
 
 
 class AboutPageTest(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        respose = self.client.get(reverse("about"))
+        self.assertEqual(respose.status_code, 200)
