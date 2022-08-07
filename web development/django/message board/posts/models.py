a32c70e30b455303078417ea19fc11fdc885bb80
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-07 13:09:58 -0500

    Updated Post model

--- web development/django/message board/posts/models.py
+++ web development/django/message board/posts/models.py
@@ -1,5 +1,9 @@
+from cgitb import text
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
     text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.text[:50]
