cc496ec1c825584e8249dd59fd45d6bd370dc76e
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-19 00:00:20 -0500

    Complement the comment on req res cycle

--- web development/flask/flasky/hello.py
+++ web development/flask/flasky/hello.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 
 # This way is created a route or view
 # A route maps a URL to a function handler in this case
+# it maps the root route to the function index.
+# The index function handles the request done to /
+# and give some html as a response.
 @app.route("/")
 def index():
     return "<h1>Hello, Flask!</h1>"
