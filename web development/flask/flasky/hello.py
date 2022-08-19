51a9366572899959eb2061626202024329b75e7a
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-18 23:40:51 -0500

    Create a Flask app instance

---
+++ web development/flask/flasky/hello.py
@@ -0,0 +1,10 @@
+""" The very first application developed with Flask.
+"""
+
+from flask import Flask
+
+# Creation af a Flask App instance
+# It requires as argument the name of the module or package
+app = Flask(__name__)
+
+
