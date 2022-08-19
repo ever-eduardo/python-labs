043d5e2c8ddcbe35a52cc43b3086252723957a05
everness <106024807+everness-dev@users.noreply.github.com>
2022-08-19 18:47:15 -0500

    Add user.<name> route

--- web development/flask/templates/hello.py
+++ web development/flask/templates/hello.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template("user.html", name=name)
