ca8657ff4b5caf967a3df5490a9435227dfbc6b0
everness-dev <106024807+everness-dev@users.noreply.github.com>
2023-02-25 14:10:11 -0500

    Add engine session

--- web development/flask/postgres-basics/app.py
+++ web development/flask/postgres-basics/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, flash
 from sqlalchemy import create_engine
-from model.person import Person
+from sqlalchemy.orm import Session
+from model.person import Person, Base
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:haskell@localhost/flasksql';
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret_string'
 
-db = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 @app.route('/')
@@ -19,12 +21,13 @@ def index():
 def create_person():
     name = request.form['name']
     color = request.form['color']
-    db.session(Person(name, color))
-    db.session.commit()
+    with Session(engine) as session:
+        person = Person(name=name, color=color)
+        session.add(person)
+        session.commit()
     return 'done'
 
 
 if __name__ == '__main__':
-    print(db)
-    db.create_all()
+    Base.metadata.create_all(engine)
     app.run()
